224a17d11d0b67e129d5fc768c59e2990c08a661
Neverhorst <tomek.qudi@gmail.com>
2020-03-24 20:52:50 +0100

    test

--- setup.py
+++ setup.py
@@ -7,13 +7,10 @@ from setuptools import setup, find_namespace_packages
 with open(os.path.join('README.md'), 'r') as file:
     long_description = file.read()
 
-with open(os.path.join('.', 'qudi', 'core', 'VERSION.txt'), 'r') as file:
-    version = file.read().strip()
-
 # packages = ['qudi', 'qudi.gui', 'qudi.logic', 'qudi.hardware', 'qudi.interface']
 
 setup(name='qudi-module-lib',
-      version=version,
+      version='1.0',
       packages=find_namespace_packages(),
       description='Qudi module library for various experiments',
       long_description=long_description,
